d11ad5f10be6aebac3b44a2bcd384cd5744c2881
Zhenduo <zhenduol@buffalo.edu>
2017-02-18 16:35:08 -0500

    new update

---
+++ src/edu/buffalo/cse116/EscapeTimeAlgorithms.java
@@ -0,0 +1,14 @@
+package edu.buffalo.cse116;
+
+public class EscapeTimeAlgorithms {
+	
+	private int _xCalc;
+	private int _yCalc;
+	private int _dist;
+	private int passes;
+	
+	public int[] pixel() {
+		
+	}
+
+}
