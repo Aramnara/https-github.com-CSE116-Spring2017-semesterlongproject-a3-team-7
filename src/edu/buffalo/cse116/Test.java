d8ea450e04a86590d4a627af7e3153fbf17b8997
Zhenduo <zhenduol@buffalo.edu>
2017-02-24 00:34:51 -0500

    TRY

--- src/edu/buffalo/cse116/Test.java
+++ src/edu/buffalo/cse116/Test.java
@@ -9,7 +9,7 @@ public class Test {
 	@org.junit.Test
 	public void test() {
 		_escape = new EscapeTimeAlgorithms();
-		assertEquals(0.4442096267, _escape.MandelbrotSet(0.3207031250000001, -0.07109374999999386), 0.001);
+		
 	}
 
 }
